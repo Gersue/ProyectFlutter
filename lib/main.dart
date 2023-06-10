72ad1aab1e5813f541468d084e374b5173eca4da
gcruz <Gersoncruz301115@gmail.com>
2023-06-10 02:19:43 -0600

    new
    Add

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
+
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Presupuesto',
-      debugShowCheckedModeBanner: false, //Propiedad para quitar el banner del debug
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -16,17 +19,104 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Gastos de Presupuestos'),
         ),
-        body: Center(
-          child: CapitalInputWidget(),
-        ),
+        body: const CapitalInputWidget(),
       ),
     );
   }
 }
 
 class CapitalInputWidget extends StatefulWidget {
+  const CapitalInputWidget({Key? key}) : super(key: key);
+
   @override
   _CapitalInputWidgetState createState() => _CapitalInputWidgetState();
+}
+
+class _CapitalInputWidgetState extends State<CapitalInputWidget> {
+  late TextEditingController _controller;
+  //Presencia ? con valores nullable
+  double? _capital;
+  //Ademas, este se utilizara para mandar mensajes de error cuando sea negativo o letras
+  String? _errorText;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  //Observemos en el TextField porque ahi es donde manejamos el error y hacemos el llamado a _saveCapital cuando presione el botton
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 300,
+        height: 200,
+        child: Card(
+          elevation: 4.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          margin: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Presupuesto',
+                      errorText: _errorText,
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    ),
+                    controller: _controller,
+                    keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                    onChanged: (value) {
+                      setState(() {
+                        try {
+                          _capital = double.parse(value);
+                          if (_capital! < 0) {
+                            _errorText = 'El capital debe ser positivo';
+                          } else {
+                            _errorText = null;
+                          }
+                        } catch (e) {
+                          _errorText = 'Ingrese un número válido';
+                        }
+                      });
+                    },
+                    onSubmitted: (value) {
+                      if (_capital != null && _capital! >= 0) {
+                        _saveCapital();
+                      }
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      onPressed: _saveCapital,
+                      child: const Text('Guardar'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+//Si no es nulo o mayor o igual a 0 mandamos a llamas la segunda vista  con Navigator push
+  void _saveCapital() {
+    if (_capital != null && _capital! >= 0) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SecondView(capital: _capital!)),
+      );
+    }
+  }
 }
 
 //Grafica
@@ -55,7 +145,7 @@ class MyPieChart extends StatelessWidget {
             child: Center(
               child: Text(
                   'Gastado: ${porcentaje.toStringAsFixed(2)}%',
-                style: TextStyle(
+                style: const TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
@@ -97,12 +187,12 @@ class _SecondViewState extends State<SecondView> {
     if (cantidad > widget.capital) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('El gasto no puede superar el presupuesto disponible'),
+          content: const Text('El gasto no puede superar el presupuesto disponible'),
           width: 200,
           backgroundColor: Colors.indigo,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(20),
-            side: BorderSide(
+            side: const BorderSide(
               color: Colors.grey,
               width: 1,
             ),
@@ -127,7 +217,7 @@ class _SecondViewState extends State<SecondView> {
         List<String> nuevoTipos = [];
 
         return AlertDialog(
-          title: Text('Agregar Gasto'),
+          title: const Text('Agregar Gasto'),
           content: SingleChildScrollView(
             child: Column(
               children: [
@@ -135,21 +225,21 @@ class _SecondViewState extends State<SecondView> {
                   onChanged: (value) {
                     nuevoNombre = value;
                   },
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Nombre',
                   ),
                 ),
-                SizedBox(height: 16),
+                const SizedBox(height: 16),
                 TextField(
                   onChanged: (value) {
                     nuevaCantidad = double.parse(value);
                   },
                   keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Cantidad',
                   ),
                 ),
-                SizedBox(height: 16),
+                const SizedBox(height: 16),
                 DropdownButtonFormField<String>(
                   onChanged: (value) {
                     if (nuevoTipos.contains(value!)) {
@@ -159,37 +249,37 @@ class _SecondViewState extends State<SecondView> {
                     }
                   },
                   value: nuevoTipos.isNotEmpty ? nuevoTipos[0] : null, // Agregar esta línea
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Tipo',
                   ),
                   items: [
-                    DropdownMenuItem(
+                    const DropdownMenuItem(
                       value: 'ahorro',
                       child: Text('Ahorro'),
                     ),
-                    DropdownMenuItem(
+                    const DropdownMenuItem(
                       value: 'comida',
                       child: Text('Comida'),
                     ),
-                    DropdownMenuItem(
+                    const DropdownMenuItem(
                       value: 'casa',
                       child: Text('Casa'),
                     ),
-                    DropdownMenuItem(
+                    const DropdownMenuItem(
                       value: 'ocio',
                       child: Text('Ocio'),
                     ),
-                    DropdownMenuItem(
+                    const DropdownMenuItem(
                       value: 'salud',
                       child: Text('Salud'),
                     ),
-                    DropdownMenuItem(
+                    const DropdownMenuItem(
                       value: 'gastos',
                       child: Text('Gastos'),
                     ),
-                    DropdownMenuItem(
+                    const DropdownMenuItem(
                       value: 'suscripciones',
-                      child: Text('Suscripciones'),
+                      child: const Text('Suscripciones'),
                     )
                   ],
                 ),
@@ -211,13 +301,13 @@ class _SecondViewState extends State<SecondView> {
                 });
                 Navigator.of(context).pop();
               },
-              child: Text('Agregar'),
+              child: const Text('Agregar'),
             ),
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text('Cancelar'),
+              child: const Text('Cancelar'),
             ),
           ],
         );
@@ -231,11 +321,11 @@ class _SecondViewState extends State<SecondView> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Control de Presupuesto'),
+        title: const Text('Control de Presupuesto'),
       ),
       body: Center(
         child: Container(
-          padding: EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
           child: SingleChildScrollView(
             child: Card(
               child: Column(
@@ -250,24 +340,24 @@ class _SecondViewState extends State<SecondView> {
                   ListTile(
                     title: Text('Gasto: $gasto'),
                   ),
-                  SizedBox(height: 16),
+                  const SizedBox(height: 16),
                   Stack(
                     alignment: Alignment.center,
                     children: [
                       Container(
                         width: 200,
                         height: 200,
-                        padding: EdgeInsets.all(16),
+                        padding: const EdgeInsets.all(16),
                         child: MyPieChart(capital: widget.capital, gasto: gasto),
                       ),
                       Container(
-                        margin: EdgeInsets.only(top: 40),
+                        margin: const EdgeInsets.only(top: 40),
                         child: Align(
                           alignment: Alignment.bottomCenter,
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.end,
                             children: [
-                              SizedBox(height: 16),
+                              const SizedBox(height: 16),
                               Visibility(
                                 visible: reiniciar,
                                 child: ElevatedButton(
@@ -277,9 +367,9 @@ class _SecondViewState extends State<SecondView> {
                                       reiniciar = false;
                                     });
                                   },
-                                  child: Text(
+                                  child: const Text(
                                     "Reiniciar",
-                                    style: TextStyle(
+                                    style: const TextStyle(
                                       fontSize: 20,
                                       fontWeight: FontWeight.bold,
                                     ),
@@ -292,21 +382,52 @@ class _SecondViewState extends State<SecondView> {
                       ),
                     ],
                   ),
-                  SizedBox(height: 16),
+                  const SizedBox(height: 16),
                   ElevatedButton(
                     onPressed: () {
-                      mostrarModalAgregarGasto();
+                      if (disponible == 0 || disponible < 0) {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('Alerta'),
+                              content: const Text('Ya no puedes agregar más gastos.'),
+                              actions: [
+                                ElevatedButton(
+                                  child: const Text('Reiniciar'),
+                                  onPressed: () {
+                                    Navigator.popUntil(context, ModalRoute.withName('/')); // Vuelve a la primera vista
+                                    setState(() {
+                                      gasto = 0.0; // Reinicia el gasto
+                                      listaGastos.clear(); // Borra los gastos ingresados
+                                    });
+                                  },
+                                ),
+
+                                TextButton(
+                                  child: const Text('OK'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      } else {
+                        mostrarModalAgregarGasto();
+                      }
                     },
-                    child: Text("Agregar Gasto"),
+                    child: const Text('Agregar Gasto'),
                   ),
-                  SizedBox(height: 16),
+                  const SizedBox(height: 16),
                   ListView.builder(
                     shrinkWrap: true,
                     itemCount: listaGastos.length,
                     itemBuilder: (BuildContext context, int index) {
                       Gasto gasto = listaGastos[index];
                       return Container(
-                        margin: EdgeInsets.all(8.0),
+                        margin: const EdgeInsets.all(8.0),
                         decoration: BoxDecoration(
                           border: Border.all(color: Colors.blue, width: 2.0),
                           borderRadius: BorderRadius.circular(8.0),
@@ -327,94 +448,4 @@ class _SecondViewState extends State<SecondView> {
       ),
     );
   }
-}
-
-
-
-class _CapitalInputWidgetState extends State<CapitalInputWidget> {
-  final TextEditingController _controller = TextEditingController();
-  //Presencia ? con valores nullable
-  double? _capital;
-  //Ademas, este se utilizara para mandar mensajes de error cuando sea negativo o letras
-  String? _errorText;
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  //Observemos en el TextField porque ahi es donde manejamos el error y hacemos el llamado a _saveCapital cuando presione el botton
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 300,
-        height: 200,
-        child: Card(
-          elevation: 4.0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          margin: EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Presupuesto',
-                      errorText: _errorText,
-                      contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
-                    ),
-                    controller: _controller,
-                    keyboardType: TextInputType.numberWithOptions(decimal: true),
-                    onChanged: (value) {
-                      setState(() {
-                        try {
-                          _capital = double.parse(value);
-                          if (_capital! < 0) {
-                            _errorText = 'El capital debe ser positivo';
-                          } else {
-                            _errorText = null;
-                          }
-                        } catch (e) {
-                          _errorText = 'Ingrese un número válido';
-                        }
-                      });
-                    },
-                    onSubmitted: (value) {
-                      if (_capital != null && _capital! >= 0) {
-                        _saveCapital();
-                      }
-                    },
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: ElevatedButton(
-                      onPressed: _saveCapital,
-                      child: Text('Guardar'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-//Si no es nulo o mayor o igual a 0 mandamos a llamas la segunda vista  con Navigator push
-  void _saveCapital() {
-    if (_capital != null && _capital! >= 0) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => SecondView(capital: _capital!)),
-      );
-    }
-  }
-
 }

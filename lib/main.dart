796e159f1abcd642d3dad78a13408febad9b0cb1
gcruz <Gersoncruz301115@gmail.com>
2023-06-09 23:32:34 -0600

    Add
    Funcionalidad de carga de monto para verlo en grafica , ademas de agregar texfield de disponible y gastos solo resta validar

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,23 @@
-import 'package:fl_chart/fl_chart.dart';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-// este comentario para subir commit a mi rama Danny
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Presupuesto',//Propiedad para quitar el banner del debug
+      title: 'Presupuesto',
+      debugShowCheckedModeBanner: false, //Propiedad para quitar el banner del debug
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-            title: const Text('Gastos de Presupuestos')
+          title: const Text('Gastos de Presupuestos'),
         ),
-        body: const Padding(padding: EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                CapitalInputWidget()
-              ],
-            )
+        body: Center(
+          child: CapitalInputWidget(),
         ),
       ),
     );
@@ -36,8 +25,6 @@ class MyApp extends StatelessWidget {
 }
 
 class CapitalInputWidget extends StatefulWidget {
-  const CapitalInputWidget({super.key});
-
   @override
   _CapitalInputWidgetState createState() => _CapitalInputWidgetState();
 }
@@ -47,7 +34,7 @@ class MyPieChart extends StatelessWidget {
   final double capital;
   final double gasto;
 
-  const MyPieChart({super.key, required this.capital, required this.gasto});
+  const MyPieChart({required this.capital, required this.gasto});
 
   @override
   Widget build(BuildContext context) {
@@ -59,16 +46,19 @@ class MyPieChart extends StatelessWidget {
           SizedBox(
             width: double.infinity,
             height: double.infinity,
-            child: CircularProgressIndicator(
-              value: porcentaje / 100,
-              strokeWidth: 10,
+              child: CircularProgressIndicator(
+                value: porcentaje / 100,
+                strokeWidth: 10,
+              ),
             ),
-          ),
           Expanded(
             child: Center(
               child: Text(
-                '${porcentaje.toStringAsFixed(2)}%',
-                style: const TextStyle(fontSize: 32),
+                  'Gastado: ${porcentaje.toStringAsFixed(2)}%',
+                style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
               ),
             ),
           ),
@@ -82,28 +72,37 @@ class MyPieChart extends StatelessWidget {
 class SecondView extends StatefulWidget {
   final double capital;
 
-  const SecondView({super.key, required this.capital});
+  SecondView({required this.capital});
 
   @override
   _SecondViewState createState() => _SecondViewState();
+}
+//Clase Gasto
+class Gasto {
+  String nombre;
+  double monto;
+  List<String> tipos;
+
+  Gasto({required this.nombre, required this.monto, required this.tipos});
 }
 
 class _SecondViewState extends State<SecondView> {
   final TextEditingController textEditingController = TextEditingController();
   double gasto = 0.0;
+  List<Gasto> listaGastos = [];
+  bool reiniciar = false;
 
   void calcularPorcentaje() {
-    double monto = double.parse(textEditingController.text);
-    if (monto > widget.capital) {
-      //Mandar mensaje es como tipo alert
+    double cantidad = double.parse(textEditingController.text);
+    if (cantidad > widget.capital) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: const Text('El gasto no puede superar el presupuesto disponible'),
+          content: Text('El gasto no puede superar el presupuesto disponible'),
           width: 200,
           backgroundColor: Colors.indigo,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(20),
-            side: const BorderSide(
+            side: BorderSide(
               color: Colors.grey,
               width: 1,
             ),
@@ -113,83 +112,222 @@ class _SecondViewState extends State<SecondView> {
       );
     } else {
       setState(() {
-        gasto = monto;
+        gasto += cantidad; // Suma el nuevo gasto al gasto total
+        reiniciar = true;
       });
     }
   }
 
+  void mostrarModalAgregarGasto() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String nuevoNombre = '';
+        double nuevaCantidad = 0.0;
+        List<String> nuevoTipos = [];
+
+        return AlertDialog(
+          title: Text('Agregar Gasto'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  onChanged: (value) {
+                    nuevoNombre = value;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Nombre',
+                  ),
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  onChanged: (value) {
+                    nuevaCantidad = double.parse(value);
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Cantidad',
+                  ),
+                ),
+                SizedBox(height: 16),
+                DropdownButtonFormField<String>(
+                  onChanged: (value) {
+                    if (nuevoTipos.contains(value!)) {
+                      nuevoTipos.remove(value);
+                    } else {
+                      nuevoTipos.add(value);
+                    }
+                  },
+                  value: nuevoTipos.isNotEmpty ? nuevoTipos[0] : null, // Agregar esta línea
+                  decoration: InputDecoration(
+                    labelText: 'Tipo',
+                  ),
+                  items: [
+                    DropdownMenuItem(
+                      value: 'ahorro',
+                      child: Text('Ahorro'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'comida',
+                      child: Text('Comida'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'casa',
+                      child: Text('Casa'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'ocio',
+                      child: Text('Ocio'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'salud',
+                      child: Text('Salud'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'gastos',
+                      child: Text('Gastos'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'suscripciones',
+                      child: Text('Suscripciones'),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  Gasto nuevoGasto = Gasto(
+                    nombre: nuevoNombre,
+                    monto: nuevaCantidad, // Utiliza el campo monto en lugar de cantidad
+                    tipos: nuevoTipos,
+                  );
+                  listaGastos.add(nuevoGasto);
+                  gasto = listaGastos.fold(0, (total, gasto) => total + gasto.monto); // Suma los montos de la lista de gastos
+                  reiniciar = false;
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text('Agregar'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    double disponible = widget.capital - gasto; // Calcula la cantidad disponible restando el gasto de la capital
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Control de Presupuesto'),
+        title: Text('Control de Presupuesto'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Card(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Card(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   ListTile(
-                    title: Text('Capital guardado: ${widget.capital}'),
+                    title: Text('Capital: ${widget.capital}'),
                   ),
-                  const SizedBox(height: 16),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    height: MediaQuery.of(context).size.height * 0.1,
-                    padding: const EdgeInsets.all(5),
-                    child: MyPieChart(capital: widget.capital, gasto: gasto),
+                  ListTile(
+                    title: Text('Disponible: $disponible'),
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    height: MediaQuery.of(context).size.height * 0.2,
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 1.0,
+                  ListTile(
+                    title: Text('Gasto: $gasto'),
+                  ),
+                  SizedBox(height: 16),
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 200,
+                        padding: EdgeInsets.all(16),
+                        child: MyPieChart(capital: widget.capital, gasto: gasto),
                       ),
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    child:  Column(
-                      children: <Widget>[
-                        Flexible(
-                          child: TextFormField(
-                            controller: textEditingController,
-                            inputFormatters: <TextInputFormatter>[
-                              FilteringTextInputFormatter.digitsOnly,
+                      Container(
+                        margin: EdgeInsets.only(top: 40),
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              SizedBox(height: 16),
+                              Visibility(
+                                visible: reiniciar,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      gasto = 0.0;
+                                      reiniciar = false;
+                                    });
+                                  },
+                                  child: Text(
+                                    "Reiniciar",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
                             ],
-                            keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                            decoration: const InputDecoration(
-                              hintText: "Ingrese un valor",
-                            ),
                           ),
                         ),
-                        const SizedBox(height: 16),
-                        ElevatedButton(
-                          onPressed: () {
-                            calcularPorcentaje();
-                          },
-                          child: const Text("Enviar"),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      mostrarModalAgregarGasto();
+                    },
+                    child: Text("Agregar Gasto"),
+                  ),
+                  SizedBox(height: 16),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: listaGastos.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      Gasto gasto = listaGastos[index];
+                      return Container(
+                        margin: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blue, width: 2.0),
+                          borderRadius: BorderRadius.circular(8.0),
                         ),
-                      ],
-                    ),
+                        child: ListTile(
+                          title: Text(gasto.nombre),
+                          subtitle: Text('Cantidad: ${gasto.monto}'),
+                          trailing: Text('Tipo: ${gasto.tipos.join(', ')}'),
+                        ),
+                      );
+                    },
                   ),
                 ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
   }
-
-
 }
-
-
 
 
 
@@ -209,48 +347,61 @@ class _CapitalInputWidgetState extends State<CapitalInputWidget> {
   //Observemos en el TextField porque ahi es donde manejamos el error y hacemos el llamado a _saveCapital cuando presione el botton
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Presupuesto',
-                errorText: _errorText,
-                contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+    return Center(
+      child: Container(
+        width: 300,
+        height: 200,
+        child: Card(
+          elevation: 4.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          margin: EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Presupuesto',
+                      errorText: _errorText,
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                    ),
+                    controller: _controller,
+                    keyboardType: TextInputType.numberWithOptions(decimal: true),
+                    onChanged: (value) {
+                      setState(() {
+                        try {
+                          _capital = double.parse(value);
+                          if (_capital! < 0) {
+                            _errorText = 'El capital debe ser positivo';
+                          } else {
+                            _errorText = null;
+                          }
+                        } catch (e) {
+                          _errorText = 'Ingrese un número válido';
+                        }
+                      });
+                    },
+                    onSubmitted: (value) {
+                      if (_capital != null && _capital! >= 0) {
+                        _saveCapital();
+                      }
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      onPressed: _saveCapital,
+                      child: Text('Guardar'),
+                    ),
+                  ),
+                ],
               ),
-              controller: _controller,
-              keyboardType: const TextInputType.numberWithOptions(decimal: true),
-              onChanged: (value) {
-                setState(() {
-                  try {
-                    _capital = double.parse(value);
-                    if (_capital! < 0) {
-                      _errorText = 'El capital debe ser positivo';
-                    } else {
-                      _errorText = null;
-                    }
-                  } catch (e) {
-                    _errorText = 'Ingrese un número válido';
-                  }
-                });
-              },
-              onSubmitted: (value) {
-                if (_capital != null && _capital! >= 0) {
-                  _saveCapital();
-                }
-              },
             ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ElevatedButton(
-                onPressed: _saveCapital,
-                child: const Text('Guardar'),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
@@ -266,27 +417,4 @@ class _CapitalInputWidgetState extends State<CapitalInputWidget> {
     }
   }
 
-}
-
-class Example1 extends StatelessWidget {
-  const Example1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(bottom: 30.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: Text(
-              'Explore the restaurant\'s delicious menu items below!',
-              style: TextStyle(
-                fontSize: 18.0,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 }

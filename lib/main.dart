d1a27fa8215262f74595d0973976293179db1c69
Danny Osmar Sevilla Martinez <dsevilla@ni.thedataage.com>
2023-06-11 16:55:35 -0600

    add flex vista inicio

--- lib/main.dart
+++ lib/main.dart
@@ -731,64 +731,69 @@ class _CapitalInputWidgetState extends State<CapitalInputWidget> {
   Widget build(BuildContext context) {
     return Center(
       child: SizedBox(
-        width: 300,
+        width: 380,
         height: 200,
-        child: Card(
-          elevation: 4.0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          margin: const EdgeInsets.all(1.0),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(2),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.attach_money,
-                    color: Colors.green, // Puedes establecer el color del icono aquí
-                    size: 30, // Puedes ajustar el tamaño del icono aquí
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Presupuesto',
-                      errorText: _errorText,
-                      contentPadding: const EdgeInsets.symmetric(horizontal: 30.0),
-                    ),
-                    controller: _controller,
-                    keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                    onChanged: (value) {
-                      setState(() {
-                        try {
-                          _capital = double.parse(value);
-                          if (_capital! < 0) {
-                            _errorText = 'El capital debe ser positivo';
-                          } else {
-                            _errorText = null;
-                          }
-                        } catch (e) {
-                          _errorText = 'Ingrese un número válido';
-                        }
-                      });
-                    },
-                    onSubmitted: (value) {
-                      if (_capital != null && _capital! >= 0) {
-                        _saveCapital();
-                      }
-                    },
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(30.0),
-                    child: ElevatedButton(
-                      onPressed: _saveCapital,
-                      child: const Text('Guardar'),
-                    ),
-                  ),
-                ],
+        child: Column(
+          children: [
+            const Text("Bienvenido", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+            Expanded(child: Card(
+              elevation: 4.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
               ),
-            ),
-          ),
+              margin: const EdgeInsets.all(1.0),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(2),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.attach_money,
+                        color: Colors.green, // Puedes establecer el color del icono aquí
+                        size: 30, // Puedes ajustar el tamaño del icono aquí
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Presupuesto',
+                          errorText: _errorText,
+                          contentPadding: const EdgeInsets.symmetric(horizontal: 5.0),
+                        ),
+                        controller: _controller,
+                        keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                        onChanged: (value) {
+                          setState(() {
+                            try {
+                              _capital = double.parse(value);
+                              if (_capital! < 0) {
+                                _errorText = 'El capital debe ser positivo';
+                              } else {
+                                _errorText = null;
+                              }
+                            } catch (e) {
+                              _errorText = 'Ingrese un número válido';
+                            }
+                          });
+                        },
+                        onSubmitted: (value) {
+                          if (_capital != null && _capital! >= 0) {
+                            _saveCapital();
+                          }
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: _saveCapital,
+                          child: const Text('Guardar'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )),
+          ],
         ),
       ),
     );
